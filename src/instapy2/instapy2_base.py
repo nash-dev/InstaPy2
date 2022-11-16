863e05483c4b28a00b642a356cbc5747d3c998b1
Antique <official.antique@gmail.com>
2022-11-16 14:07:14 +0800

    Added fix for broken folder creation

--- src/instapy2/instapy2_base.py
+++ src/instapy2/instapy2_base.py
@@ -7,6 +7,10 @@ import os
 class InstaPy2Base:
     def login(self, username: str = None, password: str = None, verification_code: str = ''):
         self.session = Client()
+
+        if not os.path.exists(path=os.getcwd() + f'{os.path.sep}/files'):
+            os.mkdir(path=os.getcwd() + f'{os.path.sep}/files')
+
         if os.path.exists(path=os.getcwd() + f'{os.path.sep}files{os.path.sep}settings.json'):
             self.session.load_settings(path=os.getcwd() + f'{os.path.sep}files{os.path.sep}settings.json')
             logged_in = self.session.login(username=username, password=password, verification_code=verification_code)
