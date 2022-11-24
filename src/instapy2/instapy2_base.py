21da009acdc378014f72f1d8e85997a49a908f74
Antique <official.antique@gmail.com>
2022-11-24 22:03:45 +0800

    Added ability to use multiple accounts

--- src/instapy2/instapy2_base.py
+++ src/instapy2/instapy2_base.py
@@ -11,12 +11,12 @@ class InstaPy2Base:
         if not os.path.exists(path=os.getcwd() + f'{os.path.sep}/files'):
             os.mkdir(path=os.getcwd() + f'{os.path.sep}/files')
 
-        if os.path.exists(path=os.getcwd() + f'{os.path.sep}files{os.path.sep}settings.json'):
-            self.session.load_settings(path=os.getcwd() + f'{os.path.sep}files{os.path.sep}settings.json')
+        if os.path.exists(path=os.getcwd() + f'{os.path.sep}files{os.path.sep}{username}.json'):
+            self.session.load_settings(path=os.getcwd() + f'{os.path.sep}files{os.path.sep}{username}.json')
             logged_in = self.session.login(username=username, password=password, verification_code=verification_code)
         else:
             logged_in = self.session.login(username=username, password=password, verification_code=verification_code)
-            self.session.dump_settings(path=os.getcwd() + f'{os.path.sep}files{os.path.sep}settings.json')
+            self.session.dump_settings(path=os.getcwd() + f'{os.path.sep}files{os.path.sep}{username}.json')
 
         print(f'[INFO]: Successfully logged in as: {self.session.username}.' if logged_in else f'[ERROR]: Failed to log in.')
         self.configuration = Configuration(session=self.session)
