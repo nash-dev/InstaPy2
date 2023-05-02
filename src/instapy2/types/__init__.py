2b9c23181dff0a3b733cedb9a64939ddc8f3df3f
Antique <official.antique@gmail.com>
2023-05-03 02:46:08 +0800

    Screw pull requests and branching

--- src/instapy2/types/__init__.py
+++ src/instapy2/types/__init__.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
 from .commenttype import CommentType
 from .liketype import LikeType
 from .posttype import PostType
-=======
-from .comment_type import CommentType
-from .like_type import LikeType
-from .follow_type import FollowType
-from .post_type import PostType
-from .unfollow_type import UnfollowType
->>>>>>> main
